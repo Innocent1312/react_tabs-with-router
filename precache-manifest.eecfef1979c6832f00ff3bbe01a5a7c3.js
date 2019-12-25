a8d475a9c99c0eabbdbe0cb27fcef75951a87d69
Innocent1312 <anthonydev33@gmail.com>
2019-12-25 16:29:49 +0200

    Updates

---
+++ precache-manifest.eecfef1979c6832f00ff3bbe01a5a7c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a1a095c757ab94dfbf9b8d4419839c",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "8b6ccaf39411a76a1958",
+    "url": "/react_tabs-with-router/static/css/main.a56a4d26.chunk.css"
+  },
+  {
+    "revision": "3eb6d0b8af5013e18824",
+    "url": "/react_tabs-with-router/static/js/2.cc1e8166.chunk.js"
+  },
+  {
+    "revision": "8b6ccaf39411a76a1958",
+    "url": "/react_tabs-with-router/static/js/main.14c88a8d.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
